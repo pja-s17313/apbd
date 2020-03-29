e6628cf774abb77b32e7213d730ded1adc464d91
Michał Dzienisiewicz <michal@yaska.eu>
2020-03-29 12:22:58 +0200

    init + zadania 1-6

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using pjatk_apbd.Models;
+
+namespace pjatk_apbd.Controllers
+{
+  [ApiController]
+  [Route("api/students")]
+
+  public class StudentsController : ControllerBase
+  {
+    [HttpGet]
+    public string GetStudents(string orderBy)
+    {
+      return $"Kowalski, Malejwski, Andrzejewski sortowanie={orderBy}";
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetStudent(int id)
+    {
+      if (id == 1)
+      {
+        return Ok("Kowalski");
+      }
+      else if (id == 2)
+      {
+        return Ok("Malewski");
+      }
+
+      return NotFound("Nie znaleziono studenta");
+    }
+
+    [HttpPost]
+    public IActionResult CreateStudent(Student student)
+    {
+      student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+      return Ok(student);
+    }
+  }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+namespace pjatk_apbd.Models
+{
+  public class Student
+  {
+    public int IdStudent { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string IndexNumber { get; set; }
+  }
+}
